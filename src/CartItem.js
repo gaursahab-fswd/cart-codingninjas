23bcb5245cff607cbd2d78caec507f54a1c7fdc7
itprogaur <professionalcodehub@gmail.com>
2022-01-28 19:59:12 +0530

    State added increase

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,29 +1,49 @@
-import React from "react";
+import React from 'react';
 
 class CartItem extends React.Component {
-  render() {
+  constructor () {
+    super();
+    this.state = {
+      price: 999,
+      title: 'Mobile Phone',
+      qty: 1,
+      img: ''
+    }
+   // this.increaseQuantity = this.increaseQuantity.bind(this);
+  }
+    increaseQuantity =() =>{
+        console.log('this', this.state);
+}
+  render () {
+    const { price, title, qty } = this.state;
     return (
       <div className="cart-item">
         <div className="left-block">
-          <img style={styles.image}/>
+          <img style={styles.image} />
         </div>
         <div className="right-block">
-          <div style={{fontSize:25}}>Phone</div>
-          <div style={{color:'#777'}}>Rs 999</div>
-          <div style={{color:'#777'}}>Qty</div>
+          <div style={ { fontSize: 25 } }>{title}</div>
+          <div style={ { color: '#777' } }>Rs {price} </div>
+          <div style={ { color: '#777' } }>Qty: {qty} </div>
+          <div className="cart-item-actions">
+            {/* Buttons */}
+            <img alt="increase" className="action-icons" src="https://cdn-icons.flaticon.com/png/512/3303/premium/3303893.png?token=exp=1643377190~hmac=682d888fa2a77b262ac8e7861f387072" onClick={this.increaseQuantity}/>
+            <img alt="decrease" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/992/992683.png" />
+            <img alt="delete" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/1214/1214428.png" />
+          </div>
         </div>
-        <div className="action-icons"></div>
       </div>
     );
   }
 }
+
 const styles = {
   image: {
     height: 110,
     width: 110,
     borderRadius: 4,
-    background:'#ccc'
-  },
-};
+    background: '#ccc'
+  }
+}
 
 export default CartItem;
