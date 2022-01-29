d9daec6fb2c9c762e3a13f9af9353746a5750446
itprogaur <professionalcodehub@gmail.com>
2022-01-29 18:57:47 +0530

    Added props to component

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,65 +1,84 @@
-import React from "react";
+import React from 'react';
 
 class CartItem extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      price: 999,
-      title: "Mobile Phone",
-      qty: 1,
-      img: "",
-    };
-  }
+  // testing () {
+  //   const promise = new Promise((resolve, reject) => {
+  //     setTimeout(() => {
+  //       resolve('done');
+  //     }, 5000);
+  //   })
 
+  //   promise.then(() => {
+  //     // setState acts like a synchronus call
+  //     this.setState({ qty: this.state.qty + 10 });
+
+  //     this.setState({ qty: this.state.qty + 10 });
+
+  //     this.setState({ qty: this.state.qty + 10 });
+
+  //     console.log('state', this.state);
+  //   });
+  // }
   increaseQuantity = () => {
+    // this.state.qty += 1;
+    // console.log('this', this.state);
+    // setState form 1
+    // this.setState({
+    //   qty: this.state.qty + 1
+    // }, () => {});
+
+    // setState form 2 - if prevState required use this
     this.setState((prevState) => {
-      return (
-        {
-          qty: prevState.qty + 1,
-        }
-      );
+      return {
+        qty: prevState.qty + 1
+      }
     });
-  };
+  }
 
   decreaseQuantity = () => {
     const { qty } = this.state;
+
     if (qty === 0) {
       return;
     }
+    // setState form 2 - if prevState required use this
     this.setState((prevState) => {
       return {
-        qty: prevState.qty - 1,
-      };
+        qty: prevState.qty - 1
+      }
     });
-  };
-  render() {
-    const { price, title, qty } = this.state;
+  }
+  render () {
+    console.log('this.props', this.props);
+    const { price, title, qty } = this.props.product;
     return (
       <div className="cart-item">
+        {this.props.jsx}
         <div className="left-block">
           <img style={styles.image} />
         </div>
         <div className="right-block">
-          <div style={{ fontSize: 25 }}>{title}</div>
-          <div style={{ color: "#777" }}>Rs {price} </div>
-          <div style={{ color: "#777" }}>Qty: {qty} </div>
+          <div style={ { fontSize: 25 } }>{title}</div>
+          <div style={ { color: '#777' } }>Rs {price} </div>
+          <div style={ { color: '#777' } }>Qty: {qty} </div>
           <div className="cart-item-actions">
+            {/* Buttons */}
             <img
               alt="increase"
               className="action-icons"
-              src="https://img.icons8.com/pastel-glyph/344/plus--v2.png"
+              src="https://img.icons8.com/ios/344/plus--v1.png"
               onClick={this.increaseQuantity}
             />
             <img
               alt="decrease"
               className="action-icons"
-              src="https://img.icons8.com/pastel-glyph/2x/minus--v2.gif"
+              src="https://img.icons8.com/external-dreamstale-lineal-dreamstale/344/external-minus-interface-dreamstale-lineal-dreamstale.png"
               onClick={this.decreaseQuantity}
             />
             <img
               alt="delete"
               className="action-icons"
-              src="https://img.icons8.com/ios/2x/delete--v2.gif"
+              src="https://img.icons8.com/carbon-copy/344/filled-trash.png"
             />
           </div>
         </div>
@@ -73,8 +92,8 @@ const styles = {
     height: 110,
     width: 110,
     borderRadius: 4,
-    background: "#ccc",
-  },
-};
+    background: '#ccc'
+  }
+}
 
 export default CartItem;
